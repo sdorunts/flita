925d8dd97f7c54bd747b9704c2a2b5fa1cf97079
Shagen Dorunts <sdorunts@yandex.com>
2022-03-03 00:13:51 +0300

    hw 1

---
+++ hw.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <math.h>
+
+#define MAX_LENGTH 10
+
+int decimalToBinary (int decimalNumber);
+
+struct set {
+  int Array[MAX_LENGTH];
+};
+
+int main (void) {
+  int setOfDecimalNumbers[MAX_LENGTH];
+  
+  for (int i = 0; i != MAX_LENGTH; ++i)
+    setOfDecimalNumbers[i] = i;
+  
+  int setOfBinaryNumbers[MAX_LENGTH];
+
+  for (int i = 0; i != MAX_LENGTH; ++i)
+    setOfBinaryNumbers[i] = decimalToBinary(setOfDecimalNumbers[i]);
+  
+  return 0;
+}
+
+int decimalToBinary (int decimalNumber) {
+  int binaryNumber = 0; 
+  int discharge = 0;
+  
+  while (decimalNumber > 0) {
+    binaryNumber += (int) powf(10, discharge++) * (decimalNumber % 2);
+    decimalNumber /= 2;
+  }
+
+  return binaryNumber;
+}
+
+void print (int Array[], int length) {
+  for (int i = 0; i != length; ++i)
+    printf("%d ", Array[i]);
+  printf("\n");
+}
+
